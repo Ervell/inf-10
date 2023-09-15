e48360422f356e6a204b1cf133b6e86c71744510
Arseny <arsenyshafranov@yandex.ru>
2023-09-15 10:13:10 +0300

    test comment

---
+++ main.py
@@ -0,0 +1,32 @@
+print("ax^2+bx+c=0")
+print("a =")
+print("b =")
+print("c =")
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+D = (b ** 2 - 4 * c * a)
+
+if D > 0:
+    D ** 0,5
+
+else:
+    
+
+if D > 0:
+    e = (-b + D) / 2 * a
+    r = (-b - D) / 2 * a
+
+    print(e)
+    print(r)
+
+elif D == 0:
+    t = -b / 2 * a
+
+    print(t)
+
+else:
+    print("no root")
+
